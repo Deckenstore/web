1b636332de00ae44f384bcceb753d1fa0e0d4e0a
Deckenstore <thedeckenstore@gmail.com>
2025-11-10 07:40:50 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,22 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyANDJRYu6r-DczEoSscILdnCLpMCwX-UvM",
   authDomain: "payments-5802a.firebaseapp.com",
   projectId: "payments-5802a",
+  storageBucket: "payments-5802a.firebasestorage.app",
   messagingSenderId: "467232488937",
-  appId: "1:467232488937:web:79680dbf4fed78a3201f78"
+  appId: "1:467232488937:web:79680dbf4fed78a3201f78",
 });
 
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('Background Message received ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
+  console.log("Received background message: ", payload);
+  self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  });
 });
